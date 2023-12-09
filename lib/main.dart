71169cdb53e8405c2d241aa84e9b0ad2c0773e92
Iman <117748925+Aari999@users.noreply.github.com>
2023-12-09 22:25:49 +0300

    second update

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,32 @@ import 'package:todo/home.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final ThemeData kDayTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+  );
+
+  final ThemeData kNightTheme = ThemeData(
+    brightness: Brightness.dark,
+    hintColor: Colors.green,
+  );
+
+  bool isNightMode = false;
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      //theme: isNightMode? kNightTheme : kDayTheme,
+      color: Color.fromARGB(255, 184, 111, 197),
+      debugShowCheckedModeBanner: false,
       title: 'To-Do List',
       home: MyHomePage(),
     );
